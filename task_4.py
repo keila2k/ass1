6218681d9da9cc26038fb8458bc24064b1302841
Shay Ben Simon <shay.bs@spot.im>
2019-11-15 16:28:58 +0200

    task 4.1

---
+++ task_4.py
@@ -0,0 +1,16 @@
+import json
+
+
+def search(file_name, *args):
+    keywords = [arg for arg in args if arg[0] != '+' and arg[0] != '-']
+    includes = [arg[1:] for arg in args if arg[0] == '+']
+    excludes = [arg[1:] for arg in args if arg[0] == '-']
+
+    db = open(file_name, 'r')
+    for line in db:
+        if all(keyword in line for keyword in keywords) and all(include in line for include in includes) and all(exclude not in line for exclude in excludes):
+            yield json.loads(line)
+
+
+for recipe in search('20170107-061401-recipeitems.json', 'Fennel'):
+    print(recipe)
